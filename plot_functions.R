2e5a57707a9cf0b60a757b240fac8659b084e393
Jonathan Kitt <jonathan.kitt@inrae.fr>
2026-03-10 13:18:14 +0100

    Applied statistics with R -> 5.2.3 Two sample t-test: review

---
+++ plot_functions.R
@@ -0,0 +1,185 @@
+# Plot Normal PDF ----
+
+plot_norm_pdf <- function(mu = 0, sigma = 1, 
+                          sigma_range = 3, qval = NULL) {
+  
+  p <- ggplot(data = data.frame(
+    x = c(mu - sigma_range*sigma, mu + sigma_range*sigma)),
+    aes(x)
+  ) +
+    stat_function(
+      fun = dnorm,
+      args = list(mean = mu, sd = sigma),
+      linewidth = 0.8
+    ) +
+    scale_x_continuous(
+      limits = c(mu - sigma_range*sigma, mu + sigma_range*sigma),
+      breaks = seq(
+        from = ceiling(mu - sigma_range*sigma),
+        to = ceiling(mu + sigma_range*sigma),
+        by = 1
+      )
+    ) +
+    labs(title = paste0("P.D.F. ~ N(mu = ", mu, ", sigma = ", sigma, ")"),
+         y = "f(x)")
+  
+  if (!is.null(qval)) {
+    
+    p <- p +
+      geom_segment(x = qval, xend = qval,
+                   y = 0, yend = dnorm(x = qval, mean = mu, sd = sigma),
+                   color = "#00998a", linetype = "dotted") +
+      geom_segment(x = mu - sigma_range*sigma, 
+                   xend = qval,
+                   y = dnorm(x = qval, mean = mu, sd = sigma), 
+                   yend = dnorm(x = qval, mean = mu, sd = sigma),
+                   color = "#00998a", linetype = "dotted") +
+      geom_point(x = qval, 
+                 y = dnorm(x = qval, mean = mu, sd = sigma),
+                 size = 3, color = "#00998a", fill = "white", shape = 21,
+                 stroke = 2) +
+      annotate(x = mu + sigma_range*sigma,
+               y = max(dnorm(x = seq(mu - sigma_range*sigma, mu + sigma_range*sigma, 0.01), mean = mu, sd = sigma)), 
+               geom = "text",
+               label = paste0("x = ", qval),
+               hjust = 1,
+               vjust = 1) +
+      annotate(x = mu + sigma_range*sigma,
+               y = max(dnorm(x = seq(mu - sigma_range*sigma, mu + sigma_range*sigma, 0.01), mean = mu, sd = sigma)), 
+               geom = "text",
+               label = paste0("f(x) = ", round(dnorm(x = qval, mean = mu, sd = sigma), 3)),
+               hjust = 1,
+               vjust = 3)
+    }
+  
+  p
+  
+  }
+
+plot_norm_pdf(qval = 2)
+
+# Plot Normal CDF ----
+
+mu = 0
+sigma = 1
+sigma_range = 3
+from = NULL
+to = NULL
+
+plot_norm_cdf <- function(mu = 0, sigma = 1, sigma_range = 3,
+                          from = NULL, to = NULL) {
+  
+  if(is.null(from)) {
+    x1 <- mu - sigma_range*sigma
+  } else {
+    x1 <- from
+  }
+  
+  if(is.null(to)) {
+    x2 <- mu + sigma_range*sigma
+  } else {
+    x2 <- to
+  }
+  
+  p <- ggplot(data = data.frame(
+    x = c(mu - sigma_range*sigma, mu + sigma_range*sigma)),
+    aes(x)
+    ) +
+    stat_function(
+      fun = dnorm,
+      args = list(mean = mu, sd = sigma),
+      geom = "area",
+      xlim = c(x1, x2),
+      fill = "#00998a",
+      alpha = 0.5
+      ) +
+    stat_function(
+      fun = dnorm,
+      args = list(mean = mu, sd = sigma),
+      linewidth = 0.8
+      ) +
+    scale_x_continuous(
+      limits = c(mu - sigma_range*sigma, mu + sigma_range*sigma),
+      breaks = seq(
+        from = ceiling(mu - sigma_range*sigma),
+        to = ceiling(mu + sigma_range*sigma),
+        by = 1
+        )
+      ) +
+    labs(title = paste0("C.D.F. ~ N(mu = ", mu, ", sigma = ", sigma, ")"),
+         y = "f(x)")
+  
+  if (is.null(from) & is.null(to)) {
+    p <- p +
+      annotate(x = mu + sigma_range*sigma,
+               y = max(dnorm(x = seq(mu - sigma_range*sigma, mu + sigma_range*sigma, 0.01), mean = mu, sd = sigma)), 
+               geom = "text",
+               label = paste0("P(x <= ", mu + sigma_range * sigma, ") = ",
+                            pnorm(q = Inf, mean = mu, sd = sigma)),
+               hjust = 1)
+    }
+  
+  if (is.null(from) & !is.null(to)) {
+    p <- p +
+      annotate(x = mu + sigma_range*sigma,
+               y = max(dnorm(x = seq(mu - sigma_range*sigma, mu + sigma_range*sigma, 0.01), mean = mu, sd = sigma)), 
+               geom = "text",
+               label = paste0("P(x <= ", to, ") = ",
+                              round(pnorm(q = to, mean = mu, sd = sigma), 3)),
+               hjust = 1)
+    }
+  
+  if (!is.null(from) & is.null(to)) {
+    p <- p +
+      annotate(x = mu + sigma_range*sigma,
+               y = max(dnorm(x = seq(mu - sigma_range*sigma, mu + sigma_range*sigma, 0.01), mean = mu, sd = sigma)), 
+               geom = "text",
+               label = paste0("P(", from, " <= x) = ",
+                              round(1 - pnorm(q = from, mean = mu, sd = sigma), 3)),
+               hjust = 1)
+    
+    }
+  
+  if (!is.null(from) & !is.null(to)) {
+    p <- p +
+      annotate(x = mu + sigma_range*sigma,
+               y = max(dnorm(x = seq(mu - sigma_range*sigma, mu + sigma_range*sigma, 0.01), mean = mu, sd = sigma)), 
+               geom = "text",
+               label = paste0("P(", from, " <= x <= ", to, ") = ",
+                              round(pnorm(q = to, mean = mu, sd = sigma) - pnorm(q = from, mean = mu, sd = sigma), 3)),
+               hjust = 1)
+    }
+  
+  p
+  
+  }
+
+# Plot Normal quantile ----
+
+plot_norm_quantile <- function(mu = 0, sigma = 1, sigma_range = 3, p_value = NULL) {
+  
+  p <- ggplot(data = data.frame(
+    x = c(mu - sigma_range*sigma, mu + sigma_range*sigma)),
+    aes(x)
+  ) +
+    stat_function(
+      fun = dnorm,
+      args = list(mean = mu, sd = sigma),
+      linewidth = 0.8
+    ) +
+    scale_x_continuous(
+      limits = c(mu - sigma_range*sigma, mu + sigma_range*sigma),
+      breaks = seq(
+        from = ceiling(mu - sigma_range*sigma),
+        to = ceiling(mu + sigma_range*sigma),
+        by = 1
+      )
+    ) +
+    labs(title = paste0("Quantile ~ N(mu = ", mu, ", sigma = ", sigma, ")"),
+         y = "f(x)")
+  
+  p
+  
+}
+
+plot_norm_quantile()
