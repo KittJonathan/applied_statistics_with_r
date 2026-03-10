335b3f3c752eca8728fb242dfdd8261e0a323862
Jonathan Kitt <jonathan.kitt@inrae.fr>
2026-03-10 15:18:43 +0100

    plot distributions functions

--- plot_functions.R
+++ plot_functions.R
@@ -56,15 +56,10 @@ plot_norm_pdf <- function(mu = 0, sigma = 1,
   
   }
 
-plot_norm_pdf(qval = 2)
+plot_norm_pdf()
+plot_norm_pdf(qval = 0)
 
 # Plot Normal CDF ----
-
-mu = 0
-sigma = 1
-sigma_range = 3
-from = NULL
-to = NULL
 
 plot_norm_cdf <- function(mu = 0, sigma = 1, sigma_range = 3,
                           from = NULL, to = NULL) {
@@ -154,32 +149,41 @@ plot_norm_cdf <- function(mu = 0, sigma = 1, sigma_range = 3,
   
   }
 
-# Plot Normal quantile ----
+plot_norm_cdf()
+plot_norm_cdf(from = -1)
+plot_norm_cdf(from = -1, to = 1)
 
-plot_norm_quantile <- function(mu = 0, sigma = 1, sigma_range = 3, p_value = NULL) {
-  
-  p <- ggplot(data = data.frame(
-    x = c(mu - sigma_range*sigma, mu + sigma_range*sigma)),
-    aes(x)
-  ) +
-    stat_function(
-      fun = dnorm,
-      args = list(mean = mu, sd = sigma),
-      linewidth = 0.8
-    ) +
-    scale_x_continuous(
-      limits = c(mu - sigma_range*sigma, mu + sigma_range*sigma),
-      breaks = seq(
-        from = ceiling(mu - sigma_range*sigma),
-        to = ceiling(mu + sigma_range*sigma),
-        by = 1
-      )
-    ) +
-    labs(title = paste0("Quantile ~ N(mu = ", mu, ", sigma = ", sigma, ")"),
-         y = "f(x)")
-  
-  p
-  
-}
+# Plot Binomial PDF ----
 
-plot_norm_quantile()
+size = 100
+prob <- 0.5
+qval <- 50
+
+df <- tibble(
+  x = 0:size,
+  prob = dbinom(x = 0:size, size = size, prob = 0.5)
+) |> 
+  mutate(qval = ifelse(x == qval, 1, 0)
+         )
+
+ggplot(data = df) +
+  geom_rect(aes(xmin = x - 0.2, xmax = x + 0.2,
+                ymin = 0, ymax = prob,
+                fill = factor(qval)),
+            show.legend = FALSE
+            )
+
+dbinom(x = 0:size, size = 10, prob = 0.5)
+pbinom(q = 2, size = 10, prob = 0.5)
+
+sum(dbinom(x = 0:size, size = 10, prob = 0.5)[1:3])
+
+ggplot(data = data.frame(
+  x = 0:size),
+  aes(x)
+) +
+  stat_function(
+    fun = dbinom,
+    args = list(size = size, prob = prob),
+    linewidth = 0.8
+  )
